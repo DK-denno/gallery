8ea24e8d97b7af40b3f1d42336197a3a17c604f6
dk-denno <dennisveer27@gmail.com>
2018-09-28 13:08:47 +0300

    added location id filters

--- gall/views.py
+++ gall/views.py
@@ -10,8 +10,8 @@ def full_pic(request,pic_id):
     return render(request,'one.html',{'posts':posts})
 
 def dcmarv(request,location_id):
-    posts = Posts.objects.filter(category_id=category_id)
-    return render(request,'dcmarv.html',{'posts':posts})
+    posts = Posts.objects.filter(location_id=location_id)
+    return render(request,'location.html',{'posts':posts})
 
 
 
